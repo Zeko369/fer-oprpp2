08fb5adca70c1105cad081a829a1e4c34a10b6c1
Fran Zekan <zekan.fran369@gmail.com>
2022-03-23 22:54:51 +0100

    [hw02] -> WIP: Start INodeVisitor

---
+++ hw02/src/main/java/hr/fer/zemris/java/custom/scripting/node/INodeVisitor.java
@@ -0,0 +1,8 @@
+package hr.fer.zemris.java.custom.scripting.node;
+
+public interface INodeVisitor {
+    public void visitTextNode(TextNode node);
+    public void visitForLoopNode(ForLoopNode node);
+    public void visitEchoNode(EchoNode node);
+    public void visitDocumentNode(DocumentNode node);
+}
