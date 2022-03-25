df0775e244ab38f89ae47be5e237db2f97711bc0
Fran Zekan <zekan.fran369@gmail.com>
2022-03-26 00:19:28 +0100

    [hw02] -> Cleanup code

--- hw02/src/main/java/hr/fer/zemris/java/custom/scripting/exec/ObjectMultistack.java
+++
@@ -1,7 +0,0 @@
-package hr.fer.zemris.java.custom.scripting.exec;
-
-import java.util.Stack;
-
-public class ObjectMultistack {
-    Stack<Object> stack = new Stack<>();
-}
--- hw02/src/main/java/hr/fer/zemris/java/custom/scripting/exec/ValueWrapper.java
+++ hw02/src/main/java/hr/fer/zemris/java/custom/scripting/exec/ValueWrapper.java
@@ -1,12 +1,6 @@
 package hr.fer.zemris.java.custom.scripting.exec;
 
-public record ValueWrapper (Object value, String name) {
-    public static final String NO_NAME_CONSTANT = "[NO_NAME]";
-
-    public ValueWrapper(Object value) {
-        this(value, ValueWrapper.NO_NAME_CONSTANT);
-    }
-
+public record ValueWrapper(Object value, String name) {
     public int asInt() {
         return (int) value;
     }
