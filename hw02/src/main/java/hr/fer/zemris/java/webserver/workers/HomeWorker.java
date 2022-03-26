c6872e3afd9352ff65d9529d6486cebaae97e8ba
Fran Zekan <zekan.fran369@gmail.com>
2022-03-26 18:14:17 +0100

    [hw02] -> Finish background color worker

--- hw02/src/main/java/hr/fer/zemris/java/webserver/workers/HomeWorker.java
+++ hw02/src/main/java/hr/fer/zemris/java/webserver/workers/HomeWorker.java
@@ -7,9 +7,6 @@ public class HomeWorker implements IWebWorker {
     @Override
     public void processRequest(RequestContext context) throws Exception {
         String bgColor = context.getPersistentParameter("bgcolor");
-        if (context.getParameter("bgcolor") != null) {
-            bgColor = context.getParameter("bgcolor");
-        }
 
         context.setTemporaryParameter("background", bgColor == null ? "7F7F7F" : bgColor);
         context.getDispatcher().dispatchRequest("/private/pages/home.smscr");
