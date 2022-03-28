caae09cd51a3edeb9229f3bd3870bd97b63a93ad
Fran Zekan <zekan.fran369@gmail.com>
2022-03-28 03:28:05 +0200

    chore: Add package script

---
+++ tools/package.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ $# -eq 0 ]] ; then
+	echo "You need to add 1 arg with foldername"
+	exit 1
+fi
+
+source ./jmbag.sh
+
+file_name="$1-$JMBAG"
+rm $file_name.zip
+
+mkdir $file_name
+cp -r $1/src $1/pom.xml $file_name
+
+zip -r $file_name.zip $file_name
+rm -rf $file_name
