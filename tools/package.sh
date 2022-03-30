320992df10ca60743132d8ec73f1dfe03b3298f9
Fran Zekan <zekan.fran369@gmail.com>
2022-03-30 16:54:54 -0500

    chore: Fix folders

--- tools/package.sh
+++ tools/package.sh
@@ -11,7 +11,7 @@ file_name="$1-$JMBAG"
 rm $file_name.zip
 
 mkdir $file_name
-cp -r $1/src $1/pom.xml $file_name
+cp -r $1/src $1/pom.xml $1/webroot $1/config $file_name
 
 zip -r $file_name.zip $file_name
 rm -rf $file_name
