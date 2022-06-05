b754b0cb5f52aad1917776780dca8b716f307a8b
Fran Zekan <zekan.fran369@gmail.com>
2022-06-05 23:33:46 +0200

    [hw05] cleanup

--- hw05/src/main/java/hr/fer/oprpp2/servlets/Author/AuthorServlet.java
+++ hw05/src/main/java/hr/fer/oprpp2/servlets/Author/AuthorServlet.java
@@ -88,7 +88,7 @@ public class AuthorServlet extends BaseServlet {
         resp.sendRedirect("/blog-app/servlet/author/" + req.getSession().getAttribute("username") + "/" + blog.getId());
     }
 
-    private void commentBlog(HttpServletRequest req, HttpServletResponse resp, RouteMatch match) throws Exception {
+    private void commentBlog(HttpServletRequest req, HttpServletResponse resp, RouteMatch match) throws ServletException, IOException {
         try {
             this.blogService.comment(match.blogId(), req.getParameter("message"), req.getParameter("email"));
         } catch (DAOException e) {
